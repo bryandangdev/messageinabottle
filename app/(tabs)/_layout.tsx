01e02b5fed22bb78b23ac216d748930f64a38174
Bryan <bryandang10@gmail.com>
2025-09-14 10:56:25 -0700

    Change navigation links to Mailbox and Beach
    - Changed navbar names
    - Changed navbar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,15 +19,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Mailbox',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="envelope.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Beach',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="water.waves" color={color} />,
         }}
       />
     </Tabs>
